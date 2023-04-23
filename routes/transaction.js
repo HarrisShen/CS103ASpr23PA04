9e44f91ec581921cb0f21ff4956bbcac69469a32
RH Shen <srh9572@gmail.com>
2023-04-23 19:29:51 -0400

    implement sort & group by

--- routes/transaction.js
+++ routes/transaction.js
@@ -2,16 +2,9 @@
   transaction.js -- Router for the Transaction
 */
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 const TransactionItem = require('../models/Transaction');
-const User = require('../models/User');
-
-
-/*
-this is a very simple server which maintains a key/value
-store using an object where the keys and values are lists of strings
-
-*/
 
 const isLoggedIn = (req,res,next) => {
   if (res.locals.loggedIn) {
@@ -25,9 +18,23 @@ const isLoggedIn = (req,res,next) => {
 router.get('/transaction',
   isLoggedIn,
   async (req, res, next) => {
-    let items = await TransactionItem.find({userId:req.user._id})
-      .sort({date:-1, createdAt:-1});
-    res.render('transaction', {items});
+    let sortBy = req.query.sortBy;
+    let items = [];
+    switch (sortBy) {
+      case "category":
+        items = await TransactionItem.find({userId:req.user._id}).sort({category:1});
+        break;
+      case "amount":
+        items = await TransactionItem.find({userId:req.user._id}).sort({amount:1});
+        break;
+      case "description":
+        items = await TransactionItem.find({userId:req.user._id}).sort({description:1});
+        break;
+      case "date":
+      default:
+        items = await TransactionItem.find({userId:req.user._id}).sort({date:-1});
+    }
+    res.render('transaction', { items });
 });
 
 // add the value in the body to the list associated to the key
@@ -71,6 +78,19 @@ router.post('/transaction/updateTransaction',
         {_id:itemId},
         {$set: {description, amount, category}});
       res.redirect('/transaction');
+});
+
+// group by category
+router.get('/transaction/ByCategory',
+  isLoggedIn,
+  async (req, res, next) => {
+    console.log("inside /transaction/ByCategory");
+    let items = await TransactionItem.aggregate([
+      {$match: {userId: new mongoose.Types.ObjectId(req.user._id)}},
+      {$group: {_id: "$category", total: {$sum: "$amount"}}},
+      {$sort: {total: -1}}
+    ]);
+    res.render('transactionByCategory', { items });
 });
 
 module.exports = router;
